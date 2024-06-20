4fafed1cd23e5ce6d651c1000fcef411e9df9388
Manuel Garcia <gilbert.mg22@gmail.com>
2024-06-20 09:46:30 +0200

    include finction to convert geojson feature to ewkt

--- frontend/stores/mapview.js
+++ frontend/stores/mapview.js
@@ -6,7 +6,22 @@
 import { defineStore, } from 'pinia'
 import setRequestConfig from './utils/setRequestConfig';
 import { useGlobalStore } from './global'
-import { da, th } from 'vuetify/locale';
+import { da, el, th } from 'vuetify/locale';
+
+function GeoToEWKT(srid, feature) {
+    if (feature.geometry.type === "Point") {
+        return `SRID=${srid};POINT(${feature.geometry.coordinates[0]} ${feature.geometry.coordinates[1]})`;
+    } else if (feature.geometry.type === "LineString") {
+        const coordinates = feature.geometry.coordinates.map(coord => coord.join(" ")).join(",");
+        return `SRID=${srid};LINESTRING(${coordinates})`;
+    } else if (feature.geometry.type === "Polygon") {
+        const coordinates = feature.geometry.coordinates.map(ring => ring.map(coord => coord.join(" ")).join(",")).join(",");
+        return `SRID=${srid};POLYGON((${coordinates}))`;
+    } else {
+        throw new Error("Unsupported geometry type");
+    }
+};
+
 
 export const useMapViewStore = defineStore('mapView', {
     state: () => ({
@@ -35,6 +50,7 @@ export const useMapViewStore = defineStore('mapView', {
                 geometries: state.geometries
             }
         }
+
     },
     actions: {
         updateGeometries(geometries) {
@@ -85,6 +101,43 @@ export const useMapViewStore = defineStore('mapView', {
                 if (error.value) {
                     throw new Error('Error creating location collection //> ', error.value)
                 }
+                
+                // Create the geometries and add them to the location collection
+                this.geometries.features.forEach( async (feature) => {
+
+                    var feature_endpoint;
+                    var feature_ewkt;
+                    if (feature.geometry.type === "Point"){
+                        feature_endpoint = `/pointfeatures/`;
+                        feature_ewkt = GeoToEWKT(4326, feature);
+                    } else if (feature.geometry.type === "LineString") {
+                        feature_endpoint = `/linefeatures/`;
+                        feature_ewkt = GeoToEWKT(4326, feature);
+                    } else if (feature.geometry.type === "Polygon") {
+                        feature_endpoint = `/polygonfeatures/`
+                        feature_ewkt = GeoToEWKT(4326, feature);
+                    }
+
+                    console.log('feature object //> ', feature_ewkt) 
+                    
+
+                    const {data, error, pending } = await useAsyncData( () => $cmsApi(feature_endpoint, 
+                        { method: 'POST', 
+                          headers: {'Content-Type': 'application/json'}, 
+                          body: {
+                            geom: feature,
+                            description: 'created from mapview store',
+                            location: location_url
+                        } 
+                        }
+                    ));
+
+                    if (error.value) {
+                        throw new Error('Error creating feature //> ', error.value)
+                    }
+                })
+
+
             } else {
                 location_url = null
             };
