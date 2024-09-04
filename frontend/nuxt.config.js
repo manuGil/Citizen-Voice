e6f5e95bdffc5a9e7c0d560a566f04116aa6b2b0
Manuel Garcia <gilbert.mg22@gmail.com>
2024-09-04 17:00:36 +0200

    upgrage to nux 3.13.1

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -6,134 +6,146 @@ const ONE_DAY = 60 * 60 * 24 * 1000
 const ONE_WEEK = ONE_DAY * 7
 
 export default defineNuxtConfig({
+  ssr: true,
 
-    ssr: true,
-    build: {
-        transpile: ["vuetify", "vue-toastification/nuxt"],
-    },
-    // For speeding up load time in development, loading in all components from external packages increases the loading time, here we disabling to auto imports
-    components: {
-        dirs: [
-            '~/components'
-        ]
-    },
-    experimental: {
-        inlineSSRStyles: false
-    },
-    nitro: {
-        compressPublicAssets: true,
-    },
-    app: {
-        head: {
-            title: "Citizen Voice",
-            link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-                {
-                    rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css',
-                    integrity: 'sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=', crossorigin: ''
-                },
-            ],
-            script: [
-                {
-                    src: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js",
-                    integrity: "sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=",
-                    crossorigin: ""
-                }
-            ]
-        }
-    },
-    css: [
-        '@mdi/font/css/materialdesignicons.min.css',
-    ],
-    imports: {
-        dirs: [
-            'stores',
-            // Scan top-level modules      
-            'composables',
-            // ... or scan modules nested one level deep with a specific name and file extension      
-            'composables/*/index.{ts,js,mjs,mts}',
-            // ... or scan all modules within given directory      
-            'composables/**'
-        ]
-    },
-    plugins: [
-        //     { src: '@/plugins/vuedraggable' }
-        // { src: '@/plugins/vue-draggable/index.js', ssr: false }
-    ],
-    vite: {
-        define: {
-            'process.env.DEBUG': false,
-        },
-        ssr: {
-            noExternal: ["vuetify"],
-        },
-    },
-    modules: [
-        // We are using @pinia/nuxt for the store
-        '@pinia/nuxt',
+  build: {
+      transpile: ["vuetify", "vue-toastification/nuxt"],
+  },
 
-        // Nuxt api party is used for the proxy API
-        'nuxt-api-party',
+  // For speeding up load time in development, loading in all components from external packages increases the loading time, here we disabling to auto imports
+  components: {
+      dirs: [
+          '~/components'
+      ]
+  },
 
-        // We are not using this now but maybe in the future
-        // '@kevinmarrec/nuxt-pwa',
+  experimental: {
+      inlineSSRStyles: false
+  },
 
-        // We are using tailwind utility classes instead of the Vuetify utility css classes
-        // More info: https://tailwindcss.nuxtjs.org/
-        '@nuxtjs/tailwindcss',
+  nitro: {
+      compressPublicAssets: true,
+  },
 
-        // We are using NuxtOpenFetch for OpenAPI clients
-        // More info: https://nuxt-open-fetch.vercel.app/setup/quick-start
-   
+  app: {
+      head: {
+          title: "Citizen Voice",
+          link: [
+              { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+              {
+                  rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css',
+                  integrity: 'sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=', crossorigin: ''
+              },
+          ],
+          script: [
+              {
+                  src: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js",
+                  integrity: "sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=",
+                  crossorigin: ""
+              }
+          ]
+      }
+  },
 
-        // this adds the vuetify vite plugin
-        // also produces type errors in the current beta release
-        async (options, nuxt) => {
-            nuxt.hooks.hook("vite:extendConfig", (config) => config.plugins.push(vuetify()));
-        },
-    ],
+  css: [
+      '@mdi/font/css/materialdesignicons.min.css',
+  ],
 
+  imports: {
+      dirs: [
+          'stores',
+          // Scan top-level modules      
+          'composables',
+          // ... or scan modules nested one level deep with a specific name and file extension      
+          'composables/*/index.{ts,js,mjs,mts}',
+          // ... or scan all modules within given directory      
+          'composables/**'
+      ]
+  },
 
-    runtimeConfig: {
-        cookieName: process.env.COOKIE_NAME || '__session',
-        cookieSecret: process.env.COOKIE_SECRET || 'secret',
-        cookieExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_DAY.toString(), 10), // 1 day
-        cookieRememberMeExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_WEEK.toString(), 10), // 7 days
-        public: {
-            baseAPI: process.env.BASE_API || 'http://localhost:3000',
-            apiHostPayment: '',
-        },
-        paymentSecretKey: '',
-    },
-    // See: https://github.com/johannschopplich/nuxt-api-party
-    apiParty: {
-        endpoints: {
-            cmsApi: { // Becomes `$cmsApi()` and useCmsApiData()
-                url: process.env.API_PARTY_CMS_URL,
-                schema: './openapi/citizenvoice/openapi.yaml'
-            },
-            authApi: { // Becomes `$authApi()` and useAuthApiData()
-                url: process.env.API_PARTY_AUTH_URL,
-                schema: './openapi/citizenvoice/openapi.yaml'
-            }
-        }
-    },
-    hooks: {
-        'pages:extend'(routes) {
-            routes.push({
-                name: "survey-design",
-                path: "/design/surveys/create",
-                file: path.resolve(__dirname, './pages/design/surveys/[_id]/index.vue')
-            });
-            return routes
-        }
-    },
-    routeRules: {
-        // Render these routes with SPA // See: https://nuxt.com/docs/guide/concepts/rendering#route-rules
-        '/design/**': { ssr: false },
-        '/design/surveys/**': { ssr: false },
-        '/user/**': { ssr: false },
-        '/survey/**': { ssr: false },
-    },
+  plugins: [
+      //     { src: '@/plugins/vuedraggable' }
+      // { src: '@/plugins/vue-draggable/index.js', ssr: false }
+  ],
+
+  vite: {
+      define: {
+          'process.env.DEBUG': false,
+      },
+      ssr: {
+          noExternal: ["vuetify"],
+      },
+  },
+
+  modules: [
+      // We are using @pinia/nuxt for the store
+      '@pinia/nuxt',
+
+      // Nuxt api party is used for the proxy API
+      'nuxt-api-party',
+
+      // We are not using this now but maybe in the future
+      // '@kevinmarrec/nuxt-pwa',
+
+      // We are using tailwind utility classes instead of the Vuetify utility css classes
+      // More info: https://tailwindcss.nuxtjs.org/
+      '@nuxtjs/tailwindcss',
+
+      // We are using NuxtOpenFetch for OpenAPI clients
+      // More info: https://nuxt-open-fetch.vercel.app/setup/quick-start
+ 
+
+      // this adds the vuetify vite plugin
+      // also produces type errors in the current beta release
+      async (options, nuxt) => {
+          nuxt.hooks.hook("vite:extendConfig", (config) => config.plugins.push(vuetify()));
+      },
+  ],
+
+  runtimeConfig: {
+      cookieName: process.env.COOKIE_NAME || '__session',
+      cookieSecret: process.env.COOKIE_SECRET || 'secret',
+      cookieExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_DAY.toString(), 10), // 1 day
+      cookieRememberMeExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_WEEK.toString(), 10), // 7 days
+      public: {
+          baseAPI: process.env.BASE_API || 'http://localhost:3000',
+          apiHostPayment: '',
+      },
+      paymentSecretKey: '',
+  },
+
+  // See: https://github.com/johannschopplich/nuxt-api-party
+  apiParty: {
+      endpoints: {
+          cmsApi: { // Becomes `$cmsApi()` and useCmsApiData()
+              url: process.env.API_PARTY_CMS_URL,
+              schema: './openapi/citizenvoice/openapi.yaml'
+          },
+          authApi: { // Becomes `$authApi()` and useAuthApiData()
+              url: process.env.API_PARTY_AUTH_URL,
+              schema: './openapi/citizenvoice/openapi.yaml'
+          }
+      }
+  },
+
+  hooks: {
+      'pages:extend'(routes) {
+          routes.push({
+              name: "survey-design",
+              path: "/design/surveys/create",
+              file: path.resolve(__dirname, './pages/design/surveys/[_id]/index.vue')
+          });
+          return routes
+      }
+  },
+
+  routeRules: {
+      // Render these routes with SPA // See: https://nuxt.com/docs/guide/concepts/rendering#route-rules
+      '/design/**': { ssr: false },
+      '/design/surveys/**': { ssr: false },
+      '/user/**': { ssr: false },
+      '/survey/**': { ssr: false },
+  },
+
+  compatibilityDate: '2024-09-04',
 })
-
