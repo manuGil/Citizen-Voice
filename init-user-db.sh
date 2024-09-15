e960c004d5f661ff556e8b0b001cceb8c2f99b2e
Manuel Garcia <gilbert.mg22@gmail.com>
2024-09-15 22:58:22 +0200

    Add init-user-db.sh script to automate database setup and user creation

---
+++ init-user-db.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER citizen WITH SUPPERUSER PASSWORD '${POSTGRES_PASSWORD}';
+	CREATE DATABASE civo OWNER TO citizen;
+	GRANT ALL PRIVILEGES ON DATABASE civo TO citizen;
+EOSQL
