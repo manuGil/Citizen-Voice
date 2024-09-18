77f09f0e5f5b38c6c2f436e3fe6e35e88e02fe0a
Manuel Garcia <gilbert.mg22@gmail.com>
2024-09-18 16:52:29 +0200

    Set default SECRET_KEY in settings.py for development purposes

--- citizenvoice/citizenvoice/settings.py
+++ citizenvoice/citizenvoice/settings.py
@@ -44,7 +44,7 @@ load_dotenv("../.env")
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'setme-in-production')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
