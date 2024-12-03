2f2cfb3e694376d998c92c682a180bfecee62e4a
Manuel Garcia <gilbert.mg22@gmail.com>
2024-12-03 11:31:49 +0100

    Remove redundant dotenv loading in settings.py

--- citizenvoice/citizenvoice/settings.py
+++ citizenvoice/citizenvoice/settings.py
@@ -37,9 +37,6 @@ load_dotenv("../.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# read environment variable form .env file
-load_dotenv("../.env")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
