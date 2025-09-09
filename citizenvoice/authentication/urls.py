600d28a8f092df5417df797d5c28e1e0158f3837
Manuel Garcia <gilbert.mg22@gmail.com>
2025-09-09 21:28:37 +0200

    refac: clean up TODO comments and update URL patterns in authentication and civilian apps

--- citizenvoice/authentication/urls.py
+++ citizenvoice/authentication/urls.py
@@ -7,7 +7,6 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
 from authentication.views import GoogleLogin
 
-# TODO: Continue here, test this code, it also contains changes with Django Allauth
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
@@ -22,6 +21,6 @@ urlpatterns = [
 
 # TODO: available ulr may change form version to version: https://github.com/iMerica/dj-rest-auth/blob/7.0.1/dj_rest_auth/urls.py
 
-# TODO: continuhe here, google loging failed, it coulb be because app can take up to 24 hours to be approved by google, so it may not work right now
+# TODO: continuhe here, google loging failed, it could be because app can take up to 24 hours to be approved by google, so it may not work right now
 # however, the token based authentication api works, and user can be registed that way.
 # learn about the the diference between token based authentication and session based authentication. Which one to use when and why
